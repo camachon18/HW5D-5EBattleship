42d508dc9ca7b39887ba96e91b45d537a15a1766
camachon18 <camachon18@up.edu>
2015-11-09 18:21:34 -0800

    Finished commenting

--- app/src/main/java/edu/up/cs301/Battleship/BattleshipGameState.java
+++ app/src/main/java/edu/up/cs301/Battleship/BattleshipGameState.java
@@ -3,41 +3,73 @@ package edu.up.cs301.Battleship;
 import edu.up.cs301.game.infoMsg.GameState;
 
 /**
- * Created by aljawad18 on 11/5/2015.
+ * @author Nathan Camacho
+ * @author Hashim AlJawad
+ * @author Kelson Sipe
+ *
+ * Last Modified: 11/9/2015
+ *
+ * Description of BattleshipGameState class:
+ * BattleshipGameState class inherits from the GameState class which is a part of the game framework.
+ * It contains all informaton pertaining to the current state of the game so that the user may make
+ * appropriate decisions. Also contains methods needed to modify the state based on user actions and
+ * other events.
+ *
  */
+
 public class BattleshipGameState extends GameState {
+    //Instance variables
     private int player1Hits;
     private int player2Hits;
-    private int playerID;
-    private boolean isHit = false;
+    private int playerID;//to keep track of whose turn it is
     private int[] shipsLife = new int[5];//human's ships life
-    private int[] computerShipsLife = new int[5];
-    private int[] shipID = {1,2,3,4,5};
-    private int[][] userGrid;
+    private int[] computerShipsLife = new int[5];//AI's ships life
+    private int[] shipID = {1,2,3,4,5};//shipID's
+    //1 corresponds to the carrier ship
+    //2 corresponds to the battleship
+    //3 corresponds to the destroyer ship
+    //4 corresponds to the submarine
+    //5 corresponds to the patrol boat
+
+    private int[][] userGrid;//grids to place both players ships on
     private int[][] computerGrid;
 
+
+    /**
+     * Description: First constructor, takes no parameters. Initializes all instance variables
+     * appropriately.
+     */
     public BattleshipGameState(){
 
         player1Hits = 0;
         player2Hits = 0;
-        playerID = 0;
+        playerID = 0;//ID 0 refers to the human player. 1 refers to the AI. Human will go first.
 
-        shipsLife[0] = 5;//carrier ship
-        shipsLife[1] = 4;//battleship
-        shipsLife[2] = 3;//destroyer ship
-        shipsLife[3] = 3;//submarine ship
-        shipsLife[4] = 2;//patrol boat
+        //All ships have a certain amount of life that will decrease by 1 if hit
+        shipsLife[0] = 5;//carrier life
+        shipsLife[1] = 4;//battleship life
+        shipsLife[2] = 3;//destroyer ship life
+        shipsLife[3] = 3;//submarine ship life
+        shipsLife[4] = 2;//patrol boat life
 
-        computerShipsLife[0] = 5;//carrier ship
-        computerShipsLife[1] = 4;//battleship
-        computerShipsLife[2] = 3;//destroyer ship
-        computerShipsLife[3] = 3;//submarine ship
-        computerShipsLife[4] = 2;//patrol boat
+        computerShipsLife[0] = 5;//carrier ship life
+        computerShipsLife[1] = 4;//battleship life
+        computerShipsLife[2] = 3;//destroyer ship life
+        computerShipsLife[3] = 3;//submarine ship life
+        computerShipsLife[4] = 2;//patrol boat life
 
+        //Initialize a 10x10 grid for each player
+        //Each location in the 10x10 grid will contain a 0,1,2, or 3
+        //0 - means that this coordinate has not been chosen by the user yet, and there is no ship located
+        //on this coordinate
+        //1 - corresponds to there being a hit on that coordinate
+        //2 - corresponds to there being a miss on that coordinate
+        //3 - corresponds to there being a ship on that coordinate
+        //Here we initialize all coordinates in both grids to 0
         userGrid = new int[10][10];
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                userGrid[i][j] = 0;//0 means nothing there and it has not been clicked on yet
+                userGrid[i][j] = 0;//0 means nothing there and it has not been touched yet
             }
         }
         computerGrid = new int[10][10];
@@ -48,6 +80,11 @@ public class BattleshipGameState extends GameState {
         }
     }
 
+    /**
+     * Description: Makes a copy of an existing game state.
+     *
+     * @param object  existing game state
+     */
     public BattleshipGameState(BattleshipGameState object){
 
         this.player1Hits = object.player1Hits;
@@ -81,6 +118,7 @@ public class BattleshipGameState extends GameState {
 
     }
 
+    //Getters and setters
     public int getPtBoatLife() {return shipsLife[4];}
 
     public void setPtBoatLife(int ptBoatLife) {this.shipsLife[4] = ptBoatLife;}
@@ -141,14 +179,6 @@ public class BattleshipGameState extends GameState {
         this.computerShipsLife[0] = carrierLife;
     }
 
-
-    public boolean getIsHit() {
-        return isHit;
-    }
-    public void setIsHit(boolean isHit) {
-        this.isHit = isHit;
-    }
-
     public int getPlayerID() {
         return playerID;
     }
@@ -176,54 +206,86 @@ public class BattleshipGameState extends GameState {
     public int[][] getUserGrid() {
         return userGrid;
     }
+
+    public void setPosInUserGrid(int row, int col, int value) {
+        this.userGrid[row][col] = value;
+    }
     public int[][] getComputerGrid() {
         return computerGrid;
     }
 
+    public void setPosInComputerGrid(int row, int col, int value) {
+        this.computerGrid[row][col] = value;
+    }
+
     public int[] getShipsLife() { return shipsLife; }
+
     public int[] getComputerShipsLife() { return computerShipsLife; }
 
+    /**
+     * Description: Checks to see if one player has hit another player's ship. If this is true, then
+     * this method will make the appropriate alterations to the game state. If the action from a player
+     * is not a hit, then it must be a miss so it will call the shipMissed method. Also, a player's
+     * turn will not change if they have not clicked on a valid location in the grid. i.e. if they clicked
+     * on a coordinate they already hit or missed.
+     *
+     * CAVEAT: This method assumes that the location where a user touches on the screen will be translated
+     * correctly to the appropriate x(row) and y(col) in their grid. This method also assumes a valid
+     * ID will be passed in (1-5).
+     *
+     * @param row coordinates for location to check
+     * @param col
+     * @param ID ID of the ship that was hit
+     */
     public void shipHit(int row, int col, int ID) {
-            if (this.playerID == 0) {//human hit computer
+            if (this.playerID == 0) {//human hit computer's ship
                 //the player should be unable to click on a spot they already missed
                 //their turn may not end until they hit or miss a new spot on the opponent's grid
                 if (computerGrid[row][col] == 3) { //if a grid position equals 3, that means a ship is there
-                    player1Hits = player1Hits + 1;
-                    this.playerID = 1;
+                    player1Hits = player1Hits + 1;//increment hits
+                    this.playerID = 1;//change the turn of a player
                     computerGrid[row][col] = 1;//1 means there is a hit in this position
+                    //loop through all ships
                     for (int i = 0; i < shipID.length; i++) {
-                        if (ID == shipID[i]) {
+                        if (ID == shipID[i]) { //decrease the life of the ship that was hit by 1
                             computerShipsLife[i] = computerShipsLife[i] - 1;
                         }
                     }
                 } else {
-                    this.shipMissed(row, col);
+                    this.shipMissed(row, col);//there is a miss in this location
                 }
-            } else { //computer hit human
-                if (userGrid[row][col] == 3) {
-                    player2Hits = player2Hits + 1;
-                    this.playerID = 0;
-                    userGrid[row][col] = 1;
+            } else { //computer hit human's ship
+                if (userGrid[row][col] == 3) {//there is a ship in this location
+                    player2Hits = player2Hits + 1;//increment hits
+                    this.playerID = 0;//change turns
+                    userGrid[row][col] = 1;//1 means there is a hit in this position
                     for (int i = 0; i < shipID.length; i++) {
-                        if (ID == shipID[i]) {
+                        if (ID == shipID[i]) {//decrease life of ship that was hit by 1
                             shipsLife[i] = shipsLife[i] - 1;
 
                         }
                     }
                 } else {
-                    this.shipMissed(row, col);
+                    this.shipMissed(row, col);//there is a miss in this location
                 }
             }
     }
+
+    /**
+     * Description: This method alters a position in a player's grid so that there is now a "miss" in
+     * that position
+     *
+     * @param row coordinates for location
+     * @param col
+     */
     public void shipMissed(int row,int col) {
-        if (this.playerID == 0){
-            //We don't want to change a spot that was hit to a miss
-            if (computerGrid[row][col] == 0) {
-                computerGrid[row][col] = 2;
-                this.playerID = 1;
+        if (this.playerID == 0){//human missed in computer's grid
+            if (computerGrid[row][col] == 0) {//0 means has not been clicked on already and no ships are there
+                computerGrid[row][col] = 2;//change to a miss (2 corresponds to miss)
+                this.playerID = 1;//change turns
             }
         }
-        else {
+        else {//computer missed in human's grid
             if (userGrid[row][col] == 0) {
                 userGrid[row][col] = 2;
                 this.playerID = 0;
@@ -231,10 +293,22 @@ public class BattleshipGameState extends GameState {
         }
 
     }
-    //assume we are being passed valid row, and col positions
+
+    /**
+     * Description: Method to set up the user's ships. Changes the value in the appropriate coordinate
+     * from 0 to 3. 3 means there is a ship in this coordinate.
+     *
+     * CAVEAT: This method assumes it is being passed valid row and col positions so that it does not
+     * go out of bounds of the 10x10 grid.
+     *
+     * @param shipNum   indicates which ship is being set up
+     * @param row       coordinates to set up ship in grid
+     * @param col
+     * @param isHorizontal  a ship can only be set up horizontally or vertically
+     */
     public void setUpUserShips(int shipNum, int row, int col, boolean isHorizontal) {
-        if (shipNum == 1) {//carrier
-            if (isHorizontal == true) {
+        if (shipNum == 1) {//carrier ship, 5 positions long
+            if (isHorizontal == true) { //the ship is positioned horizontally
 
                 userGrid[row][col] = 3;
                 userGrid[row + 1][col] = 3;
@@ -242,7 +316,7 @@ public class BattleshipGameState extends GameState {
                 userGrid[row + 3][col] = 3;
                 userGrid[row + 4][col] = 3;
 
-            } else {
+            } else {// the ship is positioned vertically
                 userGrid[row][col] = 3;
                 userGrid[row][col + 1] = 3;
                 userGrid[row][col + 2] = 3;
@@ -250,7 +324,7 @@ public class BattleshipGameState extends GameState {
                 userGrid[row][col + 4] = 3;
             }
         }
-        if (shipNum == 2) {//battleship
+        if (shipNum == 2) {//battleship, 4 positions long
             if (isHorizontal == true) {
 
                 userGrid[row][col] = 3;
@@ -265,7 +339,7 @@ public class BattleshipGameState extends GameState {
                 userGrid[row][col + 3] = 3;
             }
         }
-        if (shipNum == 3) {//destroyer
+        if (shipNum == 3) {//destroyer ship, 3 positions long
             if (isHorizontal == true) {
 
                 userGrid[row][col] = 3;
@@ -278,7 +352,7 @@ public class BattleshipGameState extends GameState {
                 userGrid[row][col + 2] = 3;
             }
         }
-        if (shipNum == 4) {//submarine
+        if (shipNum == 4) {//submarine, 3 positions long
             if (isHorizontal == true) {
 
                 userGrid[row][col] = 3;
@@ -290,7 +364,7 @@ public class BattleshipGameState extends GameState {
                 userGrid[row][col + 2] = 3;
             }
         }
-        if (shipNum == 5) {//pt boat
+        if (shipNum == 5) {//pt boat, 2 positions long
             if (isHorizontal == true) {
 
                 userGrid[row][col] = 3;
@@ -303,10 +377,22 @@ public class BattleshipGameState extends GameState {
         }
 
     }
-    //assume we are being passed valid row, and col positions
+
+    /**
+     * Description: Method to set up the computer's ships. Changes the value in the appropriate coordinate
+     * from 0 to 3. 3 means there is a ship in this coordinate.
+     *
+     * CAVEAT: This method assumes it is being passed valid row and col positions so that it does not
+     * go out of bounds of the 10x10 grid.
+     *
+     * @param shipNum   indicates which ship is being set up
+     * @param row       coordinates to set up ship in grid
+     * @param col
+     * @param isHorizontal  a ship can only be set up horizontally or vertically
+     */
     public void setUpComputerShips(int shipNum, int row, int col, boolean isHorizontal) {
-        if (shipNum == 1) {//carrier
-            if (isHorizontal == true) {
+        if (shipNum == 1) {//carrier ship, 5 positions long
+            if (isHorizontal == true) {//the ship is positioned horizontally
 
                 computerGrid[row][col] = 3;
                 computerGrid[row + 1][col] = 3;
@@ -314,7 +400,7 @@ public class BattleshipGameState extends GameState {
                 computerGrid[row + 3][col] = 3;
                 computerGrid[row + 4][col] = 3;
 
-            } else {
+            } else {//the ship is positioned vertically
                 computerGrid[row][col] = 3;
                 computerGrid[row][col + 1] = 3;
                 computerGrid[row][col + 2] = 3;
@@ -322,7 +408,7 @@ public class BattleshipGameState extends GameState {
                 computerGrid[row][col + 4] = 3;
             }
         }
-        if (shipNum == 2) {//battleship
+        if (shipNum == 2) {//battleship, 4 positions long
             if (isHorizontal == true) {
 
                 computerGrid[row][col] = 3;
@@ -337,7 +423,7 @@ public class BattleshipGameState extends GameState {
                 computerGrid[row][col + 3] = 3;
             }
         }
-        if (shipNum == 3) {//destroyer
+        if (shipNum == 3) {//destroyer ship, 3 positions long
             if (isHorizontal == true) {
 
                 computerGrid[row][col] = 3;
@@ -350,7 +436,7 @@ public class BattleshipGameState extends GameState {
                 computerGrid[row][col + 2] = 3;
             }
         }
-        if (shipNum == 4) {//submarine
+        if (shipNum == 4) {//submarine, 3 positions long
             if (isHorizontal == true) {
 
                 computerGrid[row][col] = 3;
@@ -362,7 +448,7 @@ public class BattleshipGameState extends GameState {
                 computerGrid[row][col + 2] = 3;
             }
         }
-        if (shipNum == 5) {//pt boat
+        if (shipNum == 5) {//pt boat, 2 positions long
             if (isHorizontal == true) {
 
                 computerGrid[row][col] = 3;
